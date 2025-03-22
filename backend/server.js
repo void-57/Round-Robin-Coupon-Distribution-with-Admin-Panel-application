bce010fd86c59829965e5c270f5e9b9f11fa6db8
void-57 <ruddhaani60@gmail.com>
2025-03-22 21:06:03 +0530

    refactor server setup to improve middleware organization and enable CORS

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,18 @@
-import express from "express"
-const app = express()
-import dotenv from "dotenv"
-import { v4 as uuidv4 } from "uuid";
-import db from "./db.js"
-import cookieParser from "cookie-parser";
-app.use(cookieParser());
-
-app.use((req, res, next) => {
-  if (!req.cookies.sessionId) {
-    const sessionId = uuidv4(); 
-    res.cookie("sessionId", sessionId, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); 
-  }
-  next();
-});
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
 dotenv.config();
-
-app.use(express.json())
-import userRoute from "./routes/userRoute.js"
-import adminRoute from "./routes/adminRoute.js"
-app.use('/api/user',userRoute)
-app.use('/api/admin',adminRoute)
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log("Listening on port ",PORT);
-    
-})
+import db from "./db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+app.use(cors({ origin: "http://localhost:3001", credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+import userRoute from "./routes/userRoute.js";
+import adminRoute from "./routes/adminRoute.js";
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Listening on port ", PORT);
+});
